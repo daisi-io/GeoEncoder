d01d6dae80ce291570ef4a52cdde88e975e65222
zhenshan <zhenshan@belmont.tech>
2022-02-16 21:48:24 +0800

    FIX user agent

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from geopy.geocoders import Nominatim
 
 def address_to_coordinates(address):
     # Geopy has different Geocoding services that you can choose from, including Google Maps, ArcGIS, AzureMaps, Bing, etc. Some of them require API keys, while others do not need.
+    # Complete list of encoder: https://geopy.readthedocs.io/en/latest/#module-geopy.geocoders
     # Nominatim Geocoding service, which is built on top of OpenStreetMap data
     
-    geolocator = Nominatim(user_agent="myGeocoder")
+    geolocator = Nominatim(user_agent="daisi_geocoder")
 
     location = geolocator.geocode(address)
 
@@ -19,7 +20,7 @@ def address_to_coordinates(address):
 
 
 def coordinates_to_address(latitude, longitude):
-    geolocator = Nominatim(user_agent="myGeocoder")
+    geolocator = Nominatim(user_agent="daisi_geocoder")
     location = geolocator.reverse(f"{latitude}, {longitude}")
     
     data = {
